036557ff9801a4cd7cd2e29c887e75f2535543b5
Vegolas <mateusz.tomanek@gmail.com>
2021-05-18 17:12:02 +0200

    Setup

--- CVBot/MainForm.Designer.cs
+++ CVBot/MainForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CVBot
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,17 @@ namespace CVBot
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Super bot kurwa";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
